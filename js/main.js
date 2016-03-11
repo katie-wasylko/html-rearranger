a1d7f13d601135ce452a15834173d573202fbd71
katie-wasylko <kwasylko@hotmail.com>
2016-03-11 17:00:06 -0500

    jsjsjs

--- js/main.js
+++ js/main.js
@@ -6,3 +6,14 @@ var $ultwo = $('<ul>');
 $h1one.html('Below ground veggies');
 $('body').append($h1one);
 $('body').append($ulone);
+$h1two.html('Above ground veggies');
+$('body').append($h1two);
+$('body').append($ultwo);
+
+$('ul li').each(function () {
+  if ($(this).hasClass('below')) {
+    $ulone.append($(this));
+  } else {
+    $ultwo.append($(this));
+  }
+});
