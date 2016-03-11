66f54f3a477d4ed50a368fedd732c307068eb5a0
katie-wasylko <kwasylko@hotmail.com>
2016-03-11 16:59:48 -0500

    more js

--- js/main.js
+++ js/main.js
@@ -2,3 +2,7 @@ var $h1one = $('<h1>');
 var $ulone = $('<ul>');
 var $h1two = $('<h1>');
 var $ultwo = $('<ul>');
+
+$h1one.html('Below ground veggies');
+$('body').append($h1one);
+$('body').append($ulone);
